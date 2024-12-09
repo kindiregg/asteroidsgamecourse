f8451108d5deeba084507cd98c1b1da5cdbc75a2
kindiregg <jpriveraa7@gmail.com>
2024-12-09 15:04:06 -0600

    shapes, main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     print("Starting asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     running =  True
@@ -13,6 +15,9 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        # FPS 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
