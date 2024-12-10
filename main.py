ecfdd7071c50f3aa83fead95d576d23920f03e19
kindiregg <jpriveraa7@gmail.com>
2024-12-09 18:45:31 -0600

    draw asteroids

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     print("Starting asteroids!")
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
+    
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -16,9 +28,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # FPS 60
